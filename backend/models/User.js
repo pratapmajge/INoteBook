237275a9becf7a6e65b4199de0365a995324ffa8
pratapmajge <majgepratap1504@gmail.com>
2025-03-21 20:35:31 +0530

    validation

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,5 +19,6 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 });
-
-module.exports = mongoose.model("User", UserSchema);
+const User= mongoose.model("User", UserSchema);
+User.createIndexes();
+module.exports =User
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator')
+
 
 // Create user using POST "/api/auth"
-router.post('/', async (req, res) => {
+router.post('/', [
+    body('name', ' Enter valid name').isLength({ min: 3 }),
+    body('password', ' Enter valid password').isLength({ min: 5 }),
+    body('email', ' Enter valid name').isEmail()
+], (req, res) => {
     try {
-        console.log(req.body); // Log request body
+        // console.log(req.body); // Log request body
 
-        const user = new User(req.body);
-        await user.save(); // Ensure it completes before responding
+        // const user = new User(req.body);
+        // await user.save(); // Ensure it completes before responding
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
 
-        res.status(201).json({ message: "User created successfully", user });
+        User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email
+        }).then(user => res.json({ message: "User created successfully", user }))
+        .catch(err => {
+            console.log(err)
+            res.json({error :"Email already exist" , message: err.message})
+        })
+        // res.status(201).json({ message: "User created successfully", user });
+
+
+
     } catch (error) {
         console.error("Error creating user:", error);
         res.status(500).json({ error: "Internal Server Error" });
