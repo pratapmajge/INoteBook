4d1c333acddd04fb3086adedb7e4d7e24789efed
pratapmajge <majgepratap1504@gmail.com>
2025-03-22 16:34:07 +0530

    login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator')
-const bcrypt =  require('bcrypt')
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken');
 
 
 // const JWT_SECRET="iampratapmajge$webdev@s/wdev"
 
-// Create user using POST "/api/auth" // no login required
+// Create user using POST "/api/auth/createuser" // no login required
 router.post('/createuser', [
     body('name', ' Enter valid name').isLength({ min: 3 }),
     body('password', ' Enter valid password').isLength({ min: 5 }),
@@ -30,21 +30,21 @@ router.post('/createuser', [
         }
 
         const salt = await bcrypt.genSalt(10);
-        const SecPassword = await bcrypt.hash(req.body.password , salt)
+        const SecPassword = await bcrypt.hash(req.body.password, salt)
 
-        user =await User.create({
+        user = await User.create({
             name: req.body.name,
             password: SecPassword,
             email: req.body.email
         })
-        const data={
-            user:{
-                id:user.id
+        const data = {
+            user: {
+                id: user.id
             }
         }
-        const authToken= jwt.sign(data , process.env.JWT_SECRET)
+        const authToken = jwt.sign(data, process.env.JWT_SECRET)
         // console.log(authToken)
-        res.json({authToken})
+        res.json({ authToken })
 
         // .then(user => res.json({ message: "User created successfully", user }))
         // .catch(err => {
@@ -59,5 +59,39 @@ router.post('/createuser', [
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
+
+// login user using POST "/api/auth/login" // no login required
+router.post('/createuser', [
+    body('email', ' Enter valid email').isEmail(),
+    body('password', ' password cannot be blank').exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body
+    try {
+        let user = User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ error: "User with this email does not exists" })
+        }
+        const passwordcomapre = bcrypt.compare(password, user.password)
+        if (!passwordcomapre) {
+            return res.status(400).json({ error: "User with this email does not exists" })
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, process.env.JWT_SECRET)
+        res.json({ authToken })
+    } catch (error) {
+        console.error("Error during creating user:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+
+})
 
 module.exports = router;
