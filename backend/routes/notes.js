ab103cedf62ddc2f5adabc821285c1ee352e39b4
pratapmajge <majgepratap1504@gmail.com>
2025-03-23 18:07:20 +0530

    oper-notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,22 @@
-const express=  require('express')
+const express = require('express')
 const router = express.Router()
+const fetchuser = require('../middleware/fetchuser')
+const User = require('../models/User')
+const Notes = require('../models/Notes.js');  
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req,res)=>{
-    res.json([])
+
+// route 1 : get all notes using GET "/api/auth/getalluser". login required
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    const notes=await Notes.find({user : req.user.id})
+    res.json(notes)
 })
 
+// route 2 : add new notes using GET "/api/auth/addnote". login required
+router.post('/addnote', fetchuser, async (req, res) => {
+    body('name', 'Enter a valid name (min 3 characters)').isLength({ min: 3 }),
+    body('password', 'Enter a valid password (min 5 characters)').isLength({ min: 5 }),
+    body('email', 'Enter a valid email').isEmail()
+    res.json(notes)
+})
 module.exports = router
