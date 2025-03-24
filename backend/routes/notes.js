626496b1cf230331de8615bebe39d5b849380dba
pratapmajge <majgepratap1504@gmail.com>
2025-03-24 18:55:39 +0530

    addnotes & fetchnotes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -2,21 +2,43 @@ const express = require('express')
 const router = express.Router()
 const fetchuser = require('../middleware/fetchuser')
 const User = require('../models/User')
-const Notes = require('../models/Notes.js');  
+const Notes = require('../models/Note.js');
 const { body, validationResult } = require('express-validator');
 
 
 // route 1 : get all notes using GET "/api/auth/getalluser". login required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
-    const notes=await Notes.find({user : req.user.id})
+    try{
+    const notes = await Notes.find({ user: req.user.id })
     res.json(notes)
+    }catch(error){
+        console.error("Error during user creation:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 })
 
 // route 2 : add new notes using GET "/api/auth/addnote". login required
-router.post('/addnote', fetchuser, async (req, res) => {
-    body('name', 'Enter a valid name (min 3 characters)').isLength({ min: 3 }),
-    body('password', 'Enter a valid password (min 5 characters)').isLength({ min: 5 }),
-    body('email', 'Enter a valid email').isEmail()
-    res.json(notes)
+router.post('/addnote', fetchuser, [
+    body('title', 'Enter a valid title (min 3 characters)').isLength({ min: 3 }),
+    body('description', 'Description must be in atleast 5 characters').isLength({ min: 5 })
+], async (req, res) => {
+
+    try {
+        const { title, description, tag } = req.body
+        //if there are errors, return bad request and errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Notes({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save()
+
+        res.json(savedNote)
+    } catch (error) {
+        console.error("Error during user creation:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 })
 module.exports = router
